e384f8aee3914220b04d0610c6cfb048a2923e60
Fran Bonafina <info@franbonafina.ar>
2023-02-03 02:15:16 -0300

    fist auth version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CofeeModule } from './cofee/cofee.module';
+import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CofeeModule],
+  imports: [CofeeModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: '',
+    password: 'pass123',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+  }), IamModule,],
   controllers: [AppController],
   providers: [AppService],
 })
